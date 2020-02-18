d155d244d3cc17511536919f42b59dc3e0259280
mamamamamahima <mahima.sai.iyer@gmail.com>
2020-02-18 23:21:23 +0000

    added our main strategy into team5.py

--- team5.py
+++ team5.py
@@ -6,25 +6,19 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'Team Coronavirus' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Team Coronavirus'
+strategy_name = 'Tit for Tat Backstabbing'
+strategy_description = 'Collude until opponent backstabs, if they do, copy their previous move for the rest of the game until the last round, then backstab on round 100'
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+  ''' '''
+  if len(my_history) > 0: #If not round 1
+    if len(my_history) > 98: #It's the last round, backstab
+      return 'b'
+    elif their_history[-1]=='b':
+      return 'b' # Betray if severely punished last time,
+    elif len(my_history)==0: #It's the first round, collude
+      return 'c'
+  else:
+    return 'c' # otherwise collude if opponent also colludes.
 
